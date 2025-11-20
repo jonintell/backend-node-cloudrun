2ee5df8a2bba03c859e06eb3b4ed7f54b6c709c2
jonben <eliejonathan.eb@gmail.com>
2025-11-20 12:45:55 +0200

    upload cords policy for test purposes

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,12 @@ import fileRoutes from "./routes/files";
 import { authenticate } from "./middleware/auth";
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization'],
+}));
+
 app.use(express.json());
 
 app.get("/health", (_, res) => res.send("OK"));
